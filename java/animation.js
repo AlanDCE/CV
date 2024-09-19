7c099d39332db3fc58adeb8f2f54895d2fb338cb
AlanDCE <109308526+AlanDCE@users.noreply.github.com>
2024-09-19 12:23:42 -0400

    Add files via upload

---
+++ java/animation.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-switcher').addEventListener('click', function() {
+    document.body.classList.toggle('alternate-theme');
+    document.div.classList.toggle('alternate-theme');
+});
